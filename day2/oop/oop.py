c284db7b18cfc5ff00f4efa72021f1488693c84e
ivangsp <talk2gspivan@outlook.com>
2017-07-04 08:30:12 +0300

    Updated oop

--- day2/oop/oop.py
+++ day2/oop/oop.py
@@ -1,60 +1,66 @@
 class BankAccount(object):
 
-    def __init__(self):
-        self.balance = 0
+    def __init__(self, account, person_name):
+      self.account_number == account
+      self.person_name  = person_name
+      self.balance = 0
 
     def deposit(self, amount):
         if amount < 0:
             return "Invalid deposit amount"
         else:
             self.balance+=amount 
-            return self.balance   
-
-    # def withdraw(self, amount):
-    #     if amount < 0:
-    #         raise ValueError('Insufficient balance on account')
-
-    #     self.balance-=amount
-    #     if self.balance <0:
-    #        return "insufficient balance" 
-
-    #     else:
-    #         return self.balance
+            return('{} you have successfully deposited {} and you account balance is {}'.format(self.person_name, amount, self.balance))
  
 class SavingsAccount(BankAccount):
 
-    def __init__(self):
+    def __init__(self, account, name):
+        super(BankAccount, self).__init__()
         self.balance = 5000
+        self.max_withdraw =2000000
+        self.person_name = name
+
             
     def withdraw(self, amount):
-        #self.balance = BankAccount.withdraw(amount)
-        self.balance-=amount
-        if self.balance < 500:
-            return "Cannot withdraw beyond the minimum account balance"
+        if amount <0:
+            return('Invalid amount')
+        elif amount > self.max_withdraw:
+            return('On saving_account u can not withdraw more than 2m')
         else:
-            return self.balance
+            self.balance-=amount
+            if self.balance < 5000:
+                return("{} you Cannot withdraw beyond the minimum account balance".format(self.person_name))
+            else:
+                return ('{} you have successfully withdrew {} \n your account balance is {}'.format(self.person_name, amount, self.balance))
 
 class CurrentAccount(BankAccount):
 
-    def __init__(self):
+    def __init__(self, account, name):
+        super(BankAccount, self).__init__()
         self.balance = 10000
+        self.max_withdraw = 10000000
+        self.person_name = name
 
     def withdraw(self, amount):
         
-        #self.balance = BankAccount.withdraw(amount)
-        self.balance -=amount
-        if self.balance < 10000:
-            return "Cannot withdraw beyond the min balance"
+        if amount <0:
+            return('Invalid amount')
+        elif amount > self.max_withdraw:
+            return('On current account u can not withdraw more than 10m')
         else:
-            return self.balance
+            self.balance-=amount
+            if self.balance < 10000:
+                return("{} you Cannot withdraw beyond the minimum account balance".format(self.person_name))
+            else:
+                return ('{} you have successfully withdrew {} \n your account balance is {}'.format(self.person_name, amount, self.balance))
 
-saving_account  = SavingsAccount()
-current_account = CurrentAccount()
+saving_account  = SavingsAccount('0123444', 'gsp ivan')
+current_account = CurrentAccount('018900', ' clement john')
 
-print saving_account.deposit(20000)
-print saving_account.withdraw(30000)
+print(saving_account.deposit(3000000)+"\n")
+print(saving_account.withdraw(21000000)+"\n")
 
-print current_account.deposit(10000)
-print current_account.withdraw(15000)
+print(current_account.deposit(10000))
+print(current_account.withdraw(15000))
 
         
